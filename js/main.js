3c25f24ef86a667d88d1d169a61706a86b62861f
Susanto <nerdyspook007@gmail.com>
2021-08-15 17:35:40 +0530

    feat: translation api added

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/doge.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?text=" + text
+}
+
+function clickHandler() {
+    
+    var input = txtInput.value;
+
+    fetch(getTranslationUrl(input))
+        .then(response => response.json())
+        .then(json => {
+            outputDiv.innerText = json.contents.translated;
+            console.log(json.contents.translated)
+        })
+}
+
+btnTranslate.addEventListener("click", clickHandler)
